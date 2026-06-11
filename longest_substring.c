1c37c5f1d2e04e460f72e9bc37221d0e1c99e4f9
Satyansh Chand <satyanshchand01@gmail.com>
2026-06-12 00:27:36 +0530

    Longest Substring

---
+++ longest_substring.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int lengthOfLongestSubstring(char *s)
+{
+    int strLen = sizeof(s) / sizeof(char);
+
+    int subStrLen = 1;
+
+    for (int i = 0; i < strLen; i++)
+    {
+        for (int j = i + 1; j < strLen; j++)
+        {
+            if (s[i] == s[j])
+            {
+                goto end;
+            }
+            printf("\n%c(%d) != %c(%d)", s[i], i, s[j], j);
+            subStrLen++;
+        }
+    }
+end:
+    printf("\n The length is: %d", subStrLen);
+    return 0;
+}
+int main(void)
+{
+    char str[] = "pwwkew";
+    lengthOfLongestSubstring(str);
+}
